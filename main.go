55566aaeff131d1ccca6a0ed1bd2c673729a7567
KoSakaki <sakaki@flaginc.net>
2022-08-30 18:53:16 +0900

    second commit

--- main.go
+++ main.go
@@ -8,25 +8,6 @@ import (
 	_ "runtime"
 )
 
-// func hello(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf(w, "Hello!")
-// }
-
-// func log(h http.HandlerFunc) http.HandlerFunc {
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
-// 		fmt.Println("Handler function called - " + name)
-// 		h(w, r)
-// 	}
-// }
-
-// func protected(h http.Handler) http.Handler {
-// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		// 省略
-// 		h.ServeHTTP(w ,r)
-// 	})
-// }
-
 func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	fmt.Fprintf(w, "Hello!, %s\n", p.ByName("name"))
 }
@@ -39,8 +20,6 @@ func main() {
 		Addr: "127.0.0.1:8080",
 		Handler: mux,
 	}
-
-	// http.Handle("/hello/", protected(log(hello)))
 
 	server.ListenAndServe()
 }
